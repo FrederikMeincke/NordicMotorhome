3ee1ea75d8d8cfe65b46fdd3f46121961070a655
JimmyJambino <47789013+JimmyJambino@users.noreply.github.com>
2021-05-24 12:36:16 +0200

    ddd

--- src/main/java/com/nordicmotorhome/Service/AccessoryService.java
+++ src/main/java/com/nordicmotorhome/Service/AccessoryService.java
@@ -1,11 +1,18 @@
 package com.nordicmotorhome.Service;
 
+import com.nordicmotorhome.Model.Accessory;
 import com.nordicmotorhome.Repository.AccessoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccessoryService {
     @Autowired
     AccessoryRepo accessoryRepo;
+
+    public List<Accessory> fetchAll() {
+        return accessoryRepo.fetchAll();
+    }
 }
