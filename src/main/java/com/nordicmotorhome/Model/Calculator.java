ce8b526e2bbce709004fa0eb2053d3a90a00c685
JimmyJambino <47789013+JimmyJambino@users.noreply.github.com>
2021-05-28 12:18:53 +0200

    added fields from business case
    Rental fields: halfFuel, distanceDriven
    Calculator: added calculations for new Rental fields.

--- src/main/java/com/nordicmotorhome/Model/Calculator.java
+++ src/main/java/com/nordicmotorhome/Model/Calculator.java
@@ -12,17 +12,26 @@ public class Calculator {
         LocalDate end_date = LocalDate.parse(rental.getEnd_date());
         List<Accessory> accessoryList = rental.getAccessoryList();
         int days = (int) ChronoUnit.DAYS.between(start_date, end_date);
+
         double accessoriesPrice = 0;
         if(accessoryList != null) {
             for(Accessory accessory : accessoryList) {
                 accessoriesPrice += accessory.getPrice();
             }
         }
+
+        int fuelPrice = rental.isHalfFuel() ? 70 : 0;
         double distancePrice = (rental.getPick_up_distance()+rental.getDrop_off_distance()) * 0.7; // 0.7€ per kilometer
-        double averageDistance; //TODO: add total distance / days, 1€
+        double averageDistance = rental.getDistanceDriven() / days;
+        int averageDistancePrice = 0;
+        if(averageDistance > 400) {
+            averageDistancePrice = (int) averageDistance-400; // removes any decimals after "."
+        }
         double motorhomePrice = rental.getMotorhome().getPrice();
         double seasonRate = rental.getSeason().getRate();
-        double totalPrice = (days * motorhomePrice * seasonRate + accessoriesPrice + distancePrice) * cancelFee(rental);
+        double totalPrice = (days * motorhomePrice * seasonRate + accessoriesPrice + averageDistancePrice +
+                distancePrice) * cancelFee(rental);
+
         if(totalPrice < 200) {
             return 200;
         } else {
